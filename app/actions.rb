58a30db38aeef5c9a165b0956385c256940fa55a
gingerguo <guomimai@gmail.com>
2016-08-24 17:29:02 +0000

    3

--- app/actions.rb
+++ app/actions.rb
@@ -25,7 +25,14 @@ post '/create' do
 end
 
 post '/edit' do
-  @contact = Contact.find_by()
+  @contact = Contact.find(params[:contact_id])
+  @contact.firname = params[:firstname]
+  @contact.lasname = params[:lastname]
+  @contact.email = params[:email]
+  @contact.save
+  content_type :json
+  Contact.all.to_json
+
 end
 
 post '/delete' do
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $("<section class='add-section'></section>").appendTo($('body'));
-
+  $(".add-section").empty();
   $(".add-section").append(
     $("<h3/>").text("Contact Form"), $("<p/>").text("This is my form. Please fill it out. It's awesome!"), $("<form/>",{
       action: '/create',
@@ -35,17 +35,19 @@ $(document).ready(function(){
   $("<section class='list-section'></section>").appendTo($('body'));
   $("<section class='edit-section'></section>").appendTo($('body'));
 
+   var loadList = function(results){
+     $(".list-section").empty();
+     results.forEach(function(entry){
+       $('.list-section').append("<div class='each-list' id='"+entry.id+"'>" +entry.firname+"  "+entry.lasname+"  " +entry.email+"  "+"<span class='edit-bar'><button class='button-e'>Edit</button><button class='button-d'>Delete</button></span></div>");
+       $('.edit-bar').css("display", "none");
+     });
+   };
+
   $('#show-all').on("click", function(){
-    $(".list-section").empty();
     $.ajax({
       url:"/show",
       method: "GET",
-      success: function(results){
-        results.forEach(function(entry){
-          $('.list-section').append("<div class='each-list' id='"+entry.id+"'>" + entry.firname+ "   "+ entry.lasname+ "   "+ entry.email+ "<span class='edit-bar'><button class='button-e'>Edit</button><button class='button-d'>Delete</button></span></div>");
-          $('.edit-bar').css("display", "none");
-        });
-      }
+      success: loadList
     });
   });
 
@@ -61,48 +63,81 @@ $(document).ready(function(){
   });
 
   $('.list-section').on('click', '.button-e', function(){
-    $('.edit-section').append(
+    $(".edit-section").empty();
+    var $arr = $(this).parent().parent().text().split("  ");
+    var $entry_id = $(this).parent().parent().attr("id");
+    var $firstname = $arr[0];
+    var $lastname = $arr[1];
+    var $email = $arr[2];
+    $(".edit-section").append(
+    $("<form id='myForm'/>",{
+      action: '/edit',
+      method: 'POST'
+    }).append(
       $("<input/>", {
         type: 'hidden',
-        name: 'contact_id'
+        name: 'contact_id',
+        value: $entry_id
       }),$("<br/>"),
       $("<input/>",{
         type: 'text',
         id: 'cfirname',
         name: 'firstname',
-        placeholder: 'Your first name'
+        value: $firstname
       }),$("<br/>"),
       $("<input/>", {
         type: 'text',
         id: 'clasname',
         name: 'lastname',
-        placeholder: 'Your last name'
+        value: $lastname
       }),$("<br/>"),
       $("<input/>",{
         type: 'text',
         id: 'cemail',
         name: 'email',
-        placeholder: 'Email'
+        value: $email
       }), $('<br/>'),
       $('<input/>',{
         type: 'submit',
         id: 'submit',
         value: 'Submit'
       })
-    );
-    $.ajax({
-      url: '/edit',
-      method: 'POST',
-      data: {
-
-      },
-      success: function(entry){
-        $('.list-section').append("<div class='each-list' id='"+entry.id+"'>" + entry.firname+ "   "+ entry.lasname+ "   "+ entry.email);
-      }
-    });
+    )
+  );
+    submitFunc();
   });
 
-  $('.list-section').on('click', '.button-d', function(){
-    
+  function ConvertFormToJSON(form){
+    var array = jQuery(form).serializeArray();
+    var json = {};
+
+    jQuery.each(array, function() {
+        json[this.name] = this.value || '';
+    });
+    return json;
+  }
+
+  var submitFunc = function(){ $("#myForm").on('submit', function(ev){
+    ev.preventDefault();
+    // do validations
+    data = ConvertFormToJSON('#myForm');
+    // do ajax call
+    updateContact(data, function(results){
+        loadList(results);
+    });
+  });
+};
+
+    function updateContact(user_data, _done){
+      $.ajax({
+        url: '/edit',
+        method: 'POST',
+        data: user_data,
+        success: _done
+      });
+    }
+
+  $('.list-section').on('click', '.button-d', function(){ //delete
+
   });
 });
